1ccb6d7734c87ce6c1e79bc11880285040cc2dd4
Jakub Pawlak <kubols@gmail.com>
2016-01-27 15:01:26 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/uksw/metronome/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.edu.uksw.metronome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
