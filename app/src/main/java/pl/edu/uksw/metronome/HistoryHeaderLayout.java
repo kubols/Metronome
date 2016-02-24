2831bf4f7237af85a10b5275259df8d568645cf9
Jakub Pawlak <kubols@gmail.com>
2016-02-24 14:50:18 +0100

    BeepHistory layout design update

--- app/src/main/java/pl/edu/uksw/metronome/HistoryHeaderLayout.java
+++ app/src/main/java/pl/edu/uksw/metronome/HistoryHeaderLayout.java
@@ -1,7 +1,10 @@
 package pl.edu.uksw.metronome;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
@@ -15,8 +18,9 @@ import android.widget.TextView;
  */
 public class HistoryHeaderLayout extends LinearLayout {
 
-    private final static int SIZE_SUBHEAD = 16;
-    private final static int SIZE_CAPTION = 12;
+    private final static int SIZE_SUBHEAD = 20;
+    private final static int SIZE_CAPTION = 16;
+    private final static String SUBHEAD = "subhead";
 
     public HistoryHeaderLayout(Context context, String title, String caption) {
         super(context);
@@ -26,11 +30,23 @@ public class HistoryHeaderLayout extends LinearLayout {
         ));
         setVerticalGravity(Gravity.CENTER);
         setOrientation(VERTICAL);
-        addTextView(title, SIZE_SUBHEAD, R.color.textPrimary);
-        addTextView(caption, SIZE_CAPTION, R.color.textSecondary);
-        setDivider();
+        addTextView(title, SUBHEAD, SIZE_SUBHEAD, ContextCompat.getColor(context, R.color.textPrimary));
+        addTextView(caption, SIZE_CAPTION, ContextCompat.getColor(context, R.color.textSecondary));
+        //setDivider();
     }
 
+    // add text view for header with tag to find it in view
+    private void addTextView(String string, String tag, int size, int color){
+        TextView textView = new TextView(getContext());
+        textView.setTag(tag);
+        textView.setText(string);
+        textView.setTextColor(color);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, size);
+        textView.setTypeface(Typeface.DEFAULT_BOLD);
+        this.addView(textView);
+    }
+
+    // add text view for second text
     private void addTextView(String string, int size, int color){
         TextView textView = new TextView(getContext());
         textView.setText(string);
@@ -39,10 +55,11 @@ public class HistoryHeaderLayout extends LinearLayout {
         this.addView(textView);
     }
 
+    // add line divider
     private void setDivider(){
         View line = new View(getContext());
         line.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 1));
-        line.setBackgroundColor(Color.rgb(51, 51, 51));
+        line.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.textPrimary));
         addView(line);
     }
 
