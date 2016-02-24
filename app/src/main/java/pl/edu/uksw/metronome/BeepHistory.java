46068b61c26f267598455cbf26d28710021e48d0
Herzy <herzys@hotmail.com>
2016-02-24 18:49:30 +0100

    SharedPrefs added

--- app/src/main/java/pl/edu/uksw/metronome/BeepHistory.java
+++ app/src/main/java/pl/edu/uksw/metronome/BeepHistory.java
@@ -1,5 +1,7 @@
 package pl.edu.uksw.metronome;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -14,6 +16,11 @@ import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by Herzy on 2016-02-08.
@@ -33,10 +40,14 @@ public class BeepHistory extends AppCompatActivity implements View.OnClickListen
     public AlphaAnimation fadeOut = new AlphaAnimation( 1.0f , 0.0f ) ;
     Integer todel = 0;
     Integer tmpid = -1;
+    String tempdate, today, yesterday;
 
     private final static String SUBHEAD = "subhead";
     LinearLayout my;
     //RelativeLayout my;
+
+    DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +72,11 @@ public class BeepHistory extends AppCompatActivity implements View.OnClickListen
 
         String sumhistory[] = viewSumEntries();
         Integer count = 0;
+
+        Calendar cYesterday = Calendar.getInstance();
+        cYesterday.add(Calendar.DATE, -1);
+
+
         if(sumhistory[0] == null && sumtime[0] == null)
         {
             TextView date = new TextView(this);
@@ -78,15 +94,30 @@ public class BeepHistory extends AppCompatActivity implements View.OnClickListen
                 sec = sec%60;
                 min = min%60;
 
+
                 if(hour > 0)
                     lastedtime += Long.toString(hour)+"h"+" ";
                 if(min > 0)
                     lastedtime += Long.toString(min)+"min"+" ";
                 if(sec > 0)
-                    lastedtime += Long.toString(sec)+"s"+" ";
+                    lastedtime += Long.toString(sec)+"s"+ " ";
+
 
                 // add new layout with id (count) and two textViews
-                my = new HistoryHeaderLayout(this, sumhistory[count].substring(0, 10), getString(R.string.summary) + " " + lastedtime);
+                if( df.format(Calendar.getInstance().getTime()).equals(sumhistory[count].substring(0, 10)) )
+                {
+                    tempdate = "Today";
+                    today = sumhistory[count].substring(0, 10);
+                }
+                else if ( df.format(cYesterday.getTime()).equals(sumhistory[count].substring(0, 10)) )
+                {
+                    tempdate = "Yesterday";
+                    yesterday = sumhistory[count].substring(0, 10);
+                }
+                else
+                    tempdate = sumhistory[count].substring(0, 10);
+
+                my = new HistoryHeaderLayout(this, tempdate , getString(R.string.summary) + " " + lastedtime);
                 my.setId(count);
                 my.setOnClickListener(this);
                 linearLayout.addView(my);
@@ -96,6 +127,15 @@ public class BeepHistory extends AppCompatActivity implements View.OnClickListen
 
         // CLOSE DB
         dbhelp.close();
+
+
+        SharedPreferences prefs = this.getSharedPreferences("pl.edu.uksw.metronome", Context.MODE_PRIVATE);
+
+        // If history is first time opened
+        if (!prefs.getBoolean("HistoryWasOpen", false)) {
+            Toast.makeText(this, "Tap on date to show details", Toast.LENGTH_LONG).show();
+            prefs.edit().putBoolean("HistoryWasOpen", true).commit();
+        }
     }
 
     public void onClick(View v) {
@@ -104,6 +144,11 @@ public class BeepHistory extends AppCompatActivity implements View.OnClickListen
         View child = tmpLayout.findViewWithTag(SUBHEAD);                   // find View from parent layout
         TextView tmp = (TextView)child;                                   // assign textView from child
         String tmpdate = tmp.getText().toString();                       // get String from Header textView
+
+        if(tmpdate == "Yesterday")
+            tmpdate = yesterday;
+        else if(tmpdate == "Today")
+            tmpdate = today;
 
         String history = viewAllEntries(tmpdate);
         if (history.equals(""))
@@ -131,7 +176,7 @@ public class BeepHistory extends AppCompatActivity implements View.OnClickListen
     }
 
     public String[] viewSumEntries() {
-        Cursor resultOfQuery = db.query(DBOpenHelper.TABLE_NAME, DBOpenHelper.columns, null, null, null, null, null);
+        Cursor resultOfQuery = db.query(DBOpenHelper.TABLE_NAME, DBOpenHelper.columns, null, null, null, null, "id DESC");
         resultOfQuery.moveToFirst();
         String result[] = new String[999];
         Integer i = -1;
